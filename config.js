786b76550733b4bf211c92078f4ea410be4e211a
Gamingninduwa <Induwaraheroku@gmail.com>
2024-10-28 19:23:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=HzZBXDza#AFgho2WM6mj-9VrN-O_Et2BUg9ik3PTSWqGlWO8o3QU",
-MONGODB: process.env.MONGODB || "mongodb+srv://Induwagaming:<db_9y*Mf9txfZAf4>@cluster0.ueok3.mongodb.net/",
+MONGODB: process.env.MONGODB || "",
 };
